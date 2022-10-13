e6311edd9bbca3c218cbe9d698e4eb20659d8231
Hughes, Joshua <hughes@metsci.com>
2022-10-13 10:41:24 -0500

    updated fcc

--- src/main/java/net/joshuahughes/fccamateurradio/examination/pool/function/FCC.java
+++ src/main/java/net/joshuahughes/fccamateurradio/examination/pool/function/FCC.java
@@ -1,6 +1,7 @@
 package net.joshuahughes.fccamateurradio.examination.pool.function;
 
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -9,6 +10,7 @@ import net.joshuahughes.fccamateurradio.examination.Question;
 import net.joshuahughes.fccamateurradio.examination.Utility;
 import net.joshuahughes.fccamateurradio.examination.pool.Exam;
 import net.joshuahughes.fccamateurradio.examination.pool.Pool;
+import net.joshuahughes.fccamateurradio.examination.pool.PoolImpl;
 
 public class FCC implements Creator
 {
@@ -17,7 +19,16 @@ public class FCC implements Creator
 	@Override
 	public Exam apply(Pool pool)
 	{
-		Exam exam = new Exam(pool);
+		Set<String> groups = pool.stream().map(q->q.getGroup()).distinct().collect(Collectors.toSet());
+		List<Question> qList = groups.stream().map(g->
+		{
+			List<Question> qs = pool.stream().filter(q->q.getGroup().equals(g.toString())).collect(Collectors.toList());
+			return qs.get(Utility.rnd.nextInt(qs.size()));
+		}).collect(Collectors.toList());
+		PoolImpl poolImpl = new PoolImpl();
+		poolImpl.addAll(qList);
+		poolImpl.getImages().addAll(pool.getImages());
+		Exam exam = new Exam(poolImpl);
 		List<Question> fccQuestions = IntStream.
 				range(0, cls.getQuestionCount()).
 				mapToObj(i->exam.remove(Utility.rnd.nextInt(exam.size()))).
